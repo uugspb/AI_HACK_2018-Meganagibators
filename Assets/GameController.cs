777a7add2d8318573154391cd0be936e8884fb60
Smiledir <smiledir@ast222.games>
2018-04-07 17:27:09 +0300

    Лин твин

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -1,8 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class GameController : MonoBehaviour 
 {
     private static GameController _instance;
+    
+    private List<NPC> npcs = new List<NPC>();
 
     public static GameController Instance
     {
@@ -15,7 +19,20 @@ public class GameController : MonoBehaviour
 
     public void StartWave()
     {
-        
+        var spawner = Spawner.Instance;
+        StartCoroutine(StartWaveCour(spawner));
+    }
+
+    IEnumerator StartWaveCour(Spawner spawner)
+    {
+        var spawnEnemes = 0;
+        var partTime = spawner.SpawnTime / spawner.SpawnRate;
+        while (spawnEnemes <= spawner.SpawnRate)
+        {
+            npcs.Add(spawner.SpawnBot());
+            spawnEnemes++;
+            yield return new WaitForSeconds(partTime);
+        }
     }
 
     public void Damage()
