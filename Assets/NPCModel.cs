8f5df3abfa87b97c2f7581b3ed3297ed36b49898
Smiledir <smiledir@ast222.games>
2018-04-07 16:41:32 +0300

    Spawner, NPCModel

---
+++ Assets/NPCModel.cs
@@ -0,0 +1,12 @@
+﻿public class NPCModel
+{
+	public float HP;
+	public float Armor;
+	public float Speed;
+	public float Damage;
+	public float Rate;
+	public float SpawnRate;
+	
+	public float EdDamage;
+	public float EdPassedWay;
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private Spawner _instance;
+
+    public Spawner Instance
+    {
+        get
+        {
+            if (_instance == null) _instance = FindObjectOfType<Spawner>();
+            return _instance;
+        }
+    }
+    
+    public void SpawnBot()
+    {
+        // берем модель из генетики 
+    }
+}
