f88ca3b90a40a114691d9be4761a169719535275
Glongo-PC\Glongo <glongooo4@gmail.com>
2018-04-07 16:30:51 +0300

    модели для башен

---
+++ Assets/TowerModels.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerModel{
+    public float MaxHealth = 100.0f;
+    public float Armor = 100.0f;
+    public float RegenPerSecond = 2.0f;
+}
+
+public class GunModel
+{
+    public float Damage;
+    public float linearMultiplicator;
+}
