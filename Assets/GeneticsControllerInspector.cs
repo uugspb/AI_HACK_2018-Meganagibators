b33ed92c5e2167428592c31cee79f6eb45de6111
Святослав <carcassdisassembler@gmail.com>
2018-04-08 02:24:07 +0300

    Константы, поля количество предков, количество мутаций, максимальное изменение при мутации, максимум компенсирующих полей, запуск генетики, генетика крутится в отдельном потоке, мутации, метод для запуска пересчета оценки при лвлапе юзера, создание базовой популяции, дебажный инспектор для генетики, популяция отправлена в отдельный файл

---
+++ Assets/GeneticsControllerInspector.cs
@@ -0,0 +1,57 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Reflection;
+
+[CustomEditor(typeof(GeneticsController))]
+public class GeneticsControllerInspector : Editor
+{
+    private FieldInfo currentSkillPointsField = typeof(GeneticsController).GetField("currentBotSkillPoints", BindingFlags.Instance | BindingFlags.NonPublic);
+    private FieldInfo currentBestField = typeof(GeneticsController).GetField("currentBestVariation", BindingFlags.Instance | BindingFlags.NonPublic);
+    private FieldInfo lastGivenField = typeof(GeneticsController).GetField("lastGivenVariation", BindingFlags.Instance | BindingFlags.NonPublic);
+    
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (!EditorApplication.isPlaying)
+            return;
+
+        EditorGUILayout.Separator();
+        
+        EditorGUILayout.TextField("skill points", currentSkillPointsField.GetValue(target).ToString());
+
+        EditorGUILayout.Separator();
+
+        Variation currentBest = currentBestField.GetValue(target) as Variation;
+        if (currentBest != null)
+        {
+            EditorGUILayout.LabelField("Current Best", EditorStyles.boldLabel);
+            EditorGUILayout.TextField("fitness", currentBest.fitnessValue.ToString());
+            EditorGUILayout.TextField("hp", currentBest.hpLevel.ToString() + " (" + currentBest.HPValue + ")");
+            EditorGUILayout.TextField("armor", currentBest.armorLevel.ToString() + " (" + currentBest.ArmorValue + ")");
+            EditorGUILayout.TextField("damage", currentBest.damageLevel.ToString() + " (" + currentBest.DamageValue + ")");
+            EditorGUILayout.TextField("fireRate", currentBest.fireRateLevel.ToString() + " (" + currentBest.FireRateValue + ")");
+            EditorGUILayout.TextField("speed", currentBest.speedLevel.ToString() + " (" + currentBest.SpeedValue + ")");
+            EditorGUILayout.TextField("spawnRate", currentBest.spawnRateLevel.ToString() + " (" + currentBest.SpawnRateValue + ")");
+        }
+
+        EditorGUILayout.Separator();
+
+        Variation lastGiven = lastGivenField.GetValue(target) as Variation;
+        if (lastGiven != null)
+        {
+            EditorGUILayout.LabelField("Last given", EditorStyles.boldLabel);
+            EditorGUILayout.TextField("fitness", lastGiven.fitnessValue.ToString());
+            EditorGUILayout.TextField("hp", lastGiven.hpLevel.ToString() + " (" + lastGiven.HPValue + ")");
+            EditorGUILayout.TextField("armor", lastGiven.armorLevel.ToString() + " (" + lastGiven.ArmorValue + ")");
+            EditorGUILayout.TextField("damage", lastGiven.damageLevel.ToString() + " (" + lastGiven.DamageValue + ")");
+            EditorGUILayout.TextField("fireRate", lastGiven.fireRateLevel.ToString() + " (" + lastGiven.FireRateValue + ")");
+            EditorGUILayout.TextField("speed", lastGiven.speedLevel.ToString() + " (" + lastGiven.SpeedValue + ")");
+            EditorGUILayout.TextField("spawnRate", lastGiven.spawnRateLevel.ToString() + " (" + lastGiven.SpawnRateValue + ")");
+        }
+    }
+}
+#endif
---
+++ Assets/Population.cs
@@ -0,0 +1,24 @@
+﻿public class Population
+{
+    public Variation[] parents;
+    public Variation[] variations;
+
+    public Population(Variation[] variations, Variation[] parents)
+    {
+        this.variations = variations;
+        this.parents = parents;
+    }
+
+    public Variation GetBestVariation()
+    {
+        Variation bestVariation = null;
+        for (int i = 0; i < variations.Length; i++)
+            if (!float.IsNaN(variations[i].fitnessValue)
+                && (bestVariation == null || bestVariation.fitnessValue < variations[i].fitnessValue))
+            {
+                bestVariation = variations[i];
+            }
+
+        return bestVariation;
+    }
+}
